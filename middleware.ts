393b10085e3b242ece9ee74c2a0adf43647b206c
Adnan Macanovic Omerbasic <AdnanOmerbasic99@gmail.com>
2025-02-03 00:54:40 +0100

    added proctected routes

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,40 @@
-export { auth as middleware } from "./auth"
+import { UserHasAtLeastOneRental } from "@/lib/db/queries/user-has-rentals";
+import { auth } from "./auth";
+import { NextResponse } from "next/server";
+
+const isLoggedIn = ["/sign-in", "/sign-up", "admin-dashboard"];
+const notRenting = ["/dashboard", "/admin-dashboard"];
+const notLoggedIn = ["start-rental", "/dashboard", "/admin-dashboard"];
+
+
+export default auth(async (req) => {
+  const { pathname, origin } = req.nextUrl;
+  const hasRental = await UserHasAtLeastOneRental();
+
+  console.log("Current pathname:", pathname);
+  console.log("Auth data:", req.auth);
+
+  if (req.auth && isLoggedIn.includes(pathname)) {
+    console.log("Redirecting from logged-in auth route...");
+    const newUrl = new URL("/", origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  if (req.auth && !hasRental && notRenting.includes(pathname)) {
+    console.log("Redirecting from logged-in auth route...");
+    const newUrl = new URL("/", origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  if (!req.auth && notLoggedIn.includes(pathname)) {
+    console.log("Redirecting from logged-in auth route...");
+    const newUrl = new URL("/", origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
