449352851add85aa0ca9fe097beeff3d4972c2c2
Adnan Macanovic Omerbasic <AdnanOmerbasic99@gmail.com>
2025-02-03 01:26:30 +0100

    removed dotenv

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,8 +1,5 @@
 import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
-// import { config } from "dotenv";
-
-// config({ path: ".env" });
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle({ client: pool });
